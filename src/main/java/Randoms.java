260ea847f095d6ab6c253b5cf036cecc8c75553f
Artyom Bekkuzhinov <bekkygi27@gmail.com>
2023-01-28 00:53:52 +0500

    1.3.1

---
+++ src/main/java/Randoms.java
@@ -0,0 +1,21 @@
+import java.util.Iterator;
+import java.util.Random;
+
+public class Randoms implements Iterable<Integer> {
+    protected Random random;
+    int min;
+    int max;
+
+    public Randoms(int min, int max) {
+        this.min = min;
+        this.max = max;
+        random = new Random();
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return new RandomIterator(this);
+    }
+
+
+}
